a0009563390493bd95f8593b224a184f65343503
parthiban <parthibanpragalathann@gmail.com>
2021-12-25 09:29:25 +0530

    add comment python 2 file commit

---
+++ python_comment_2.py
@@ -0,0 +1,16 @@
+'''
+comments in python
+figure out what is doing & why? program to explain in natural language what program is doing.
+comments in the source code for human read not for computers excute.
+singleline comment and multiline comment
+'''
+
+
+#single line comment
+'''
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+'''
