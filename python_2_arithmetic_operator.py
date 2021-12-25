38e7488b07034820afcb25c928e8f78fd90c455c
parthiban <parthibanpragalathann@gmail.com>
2021-12-26 00:44:36 +0530

    add arthmetic python 5th file

---
+++ python_2_arithmetic_operator.py
@@ -0,0 +1,33 @@
+#Operators are special symbols that represent computation.
+#Operands The value the operator is applied to are called Operands
+#PEMDAS is useful way to remember computation.
+# Arithmetic operators
+"""
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+**
+//	Floor division
+"""
+a=1
+b=10
+print("Addition of a,b value is ", a+b)
+print("Subtraction of a to b value is ", a-b)
+print("Multiplication of a * b value is ", a*b)
+print("Division of b/a value is ", b/a)
+print("Modulus of a%b value is ", a % b)
+print("Exponentiation of a**b value is ", a**b)
+print("Floor division of b//a value is ", b//a)
+
+'''output
+Addition of a,b value is  11
+Subtraction of a to b value is  -9
+Multiplication of a * b value is  10
+Division of b/a value is  10.0
+Modulus of a%b value is  1
+Exponentiation of a**b value is  1
+Floor division of b//a value is  10
+
+'''
