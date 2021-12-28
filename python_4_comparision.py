afd609b2aec6347243bd6eb14be70d89d8c75d18
parthiban <parthibanpragalathann@gmail.com>
2021-12-28 09:26:06 +0530

    add Comparison python 7th file

---
+++ python_4_comparision.py
@@ -0,0 +1,29 @@
+'''
+
+python comparison operator also called relational operator,
+compares the values of two operands and returns True or False,
+in view of whether the condition is met.
+
+'''
+
+# Comparison Operators or Relational Operators
+
+"""
+==	Equal
+!=	Not equal
+>	Greater than
+<	Less than
+>=	Greater than or equal to
+<=	Less than or equal to
+
+"""
+
+com_val_a=10
+com_val_b=20
+print("Comparison Operators Output ")
+print("Comparison Equal Operators 10 == 20  is ", com_val_a==com_val_b)
+print("Comparison Not Equal Operators 10 != 20  is ", com_val_a!=com_val_b)
+print("Comparison Less than Operators 10 < 20  is ", com_val_a<com_val_b)
+print("Comparison Greater than Operators 10 > 20  is ", com_val_a>com_val_b)
+print("Comparison Less than Equal Operators 10 <= 20  is ", com_val_a<=com_val_b)
+print("Comparison Greater than Equal Operators 10 >= 20  is ", com_val_a>=com_val_b)
