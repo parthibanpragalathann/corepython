7a1eab5ce0ce97289d7e115e7011c468a828f9a2
parthiban <parthibanpragalathann@gmail.com>
2021-12-28 09:04:58 +0530

    add Assignment python 6th file

---
+++ python_3_assignment.py
@@ -0,0 +1,34 @@
+'''
+
+Assignment Operators are used to assigning value to a variable
+
+'''
+# Assignment Operators
+
+"""
+=   Assignment
++=	Addition
+-=	Subtraction
+*=	Multiplication
+/=	Division
+%=	Modulus
+**=	Exponentiation
+//=	Floor division
+"""
+ass_val=110
+print("Assignment : ass_val=110 : ", ass_val)
+ass_val += 10
+print("Assignment Addition : ass_val += 10 : ", ass_val)
+ass_val -= 100
+print("Assignment Subtraction : ass_val -= 100 : ", ass_val)
+ass_val *= 10
+print("Assignment Multiplication : ass_val *= 10 : ", ass_val)
+ass_val /= 10
+print("Assignment Division : ass_val /= 10 : ", ass_val)
+ass_val %= 2
+print("Assignment Modulus : ass_val %= 2 : ", ass_val)
+ass_val = 10
+ass_val **= 5
+print("Assignment Exponentiation : ass_val **= 5 : ", ass_val)
+ass_val //= 2
+print("Assignment Floor division : ass_val //= 2 : ", ass_val)
