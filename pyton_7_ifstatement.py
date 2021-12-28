9d64a323faa7185ab92c225b9ef0a589c97503d7
parthiban <parthibanpragalathann@gmail.com>
2021-12-28 11:56:39 +0530

    add If statement python 8th file

---
+++ pyton_7_ifstatement.py
@@ -0,0 +1,10 @@
+'''
+the boolean statements expression after is called condition If true then the indent statement get excuted.
+if not nothing happens
+'''
+
+#write a program for check get the number positive
+if_val = int(input("ENTER THE NUMBER TO GET RESULT "))
+
+if if_val>0:
+    print("Your ", if_val, " IS POSITIVE")
