eeedc3eec19ed7dc5f171979ffa282fe7abca058
parthiban <parthibanpragalathann@gmail.com>
2021-12-30 10:32:02 +0530

    add break using while loop python file 13

---
+++ python_13_breakwhile.py
@@ -0,0 +1,18 @@
+'''
+Break Statement
+
+When meeting the break report Stopped the whole iteration.
+break statement used to exit the entire loop Otherwise the loop can continue to process it.
+
+'''
+
+#work break statement in while loop when iterate value equal to six the loop is terminated otherwise it's work continously.
+
+
+i=1
+
+while i<10:
+    if i==6:
+        break
+    i += 1
+    print(i)
