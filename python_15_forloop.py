ca968a9597a9af2278b14693fa775b6f8207b5cb
parthiban <parthibanpragalathann@gmail.com>
2021-12-30 11:38:47 +0530

    add  for loop python file 15

---
+++ python_15_forloop.py
@@ -0,0 +1,29 @@
+'''
+For loop is a  definite iteration
+
+'''
+
+for i in range(6):     # 0 to given value - 1
+    print(i)
+
+#output
+"""
+0
+1
+2
+3
+4
+5
+"""
+
+for i in range(1, 12, 3): #value one to increment 3 by 3
+    print(i)
+
+#output
+
+"""
+1
+4
+7
+10
+"""
