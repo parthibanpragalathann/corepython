e25b3a4815d2de8cf22e47980ab5cacc2cc49b5f
parthiban <parthibanpragalathann@gmail.com>
2021-12-30 10:46:05 +0530

    add range python file 14

---
+++ python_14_range_seq.py
@@ -0,0 +1,27 @@
+'''
+
+range function it's return the sequence of numbers,by default starts with 0
+
+'''
+
+#print the sequence of value 0 to given value -1
+print(list(range(5)))
+
+#output
+[0, 1, 2, 3, 4]
+
+#print the sequence of value 1 to given value -1
+print(list(range(1, 5)))
+
+#output
+[1, 2, 3, 4]
+#print the sequence of even number  value 0 to given value -1 increment value of two by two
+print(list(range(0, 5, 2)))
+
+#output
+[0, 2, 4]
+#print the sequence of add number value 1 to given value -1 increment value of two by two
+print(list(range(1, 5, 2)))
+
+#output
+[1, 3]
