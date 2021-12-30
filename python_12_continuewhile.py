d0fb57ff4fc7bfbab238ef160f7c6014947658bd
parthiban <parthibanpragalathann@gmail.com>
2021-12-30 10:07:11 +0530

    add continue using while loop python file 12

---
+++ python_12_continuewhile.py
@@ -0,0 +1,19 @@
+'''
+
+Continue using while loop in python
+
+continue statement used to move the control towards the top of the next iteration in the loop.
+it's used bothe while and for loop.
+
+'''
+
+#write the program to print the add number using continue statement
+i=1
+while i<=10:
+    if i % 2==0:
+        i=i+1
+        continue
+    print(i, "this is add number ")
+    i+=1
+
+
