0aee85a19e3d1b99cb18f1aec8f87d284c9f26a9
parthiban <parthibanpragalathann@gmail.com>
2022-01-01 20:26:06 +0530

    add math function python file 25

---
+++ python_25_math.py
@@ -0,0 +1,34 @@
+'''
+MATH FUNCTION IN PYTHON
+
+The math module is used to access mathematical functions in the Python.
+All methods of this functions are used for integer or real type objects, not for complex numbers.
+'''
+
+
+#Source Code
+import math
+print("square root of ", math.sqrt(8))
+print("power of ", math.pow(6, 2))
+# 5! => 1*2*3*4*5
+print("factorial  is  ", math.factorial(5))
+print("greatest whole number of given number ", math.ceil(1.55555))
+print("eliminate the digit number ", math.floor(1.55555))
+print("absolute numbers only return positive number", math.fabs(-5))
+print("log2 of 2 is ", math.log2(2))
+print("log10 of 2 is ", math.log10(2))
+print("constant pi value ", math.pi)
+print("constant e value ", math.e)
+
+'''
+OUTPUT
+square root of  2.8284271247461903
+power of  36.0
+factorial  is   120
+greatest whole number of given number  2
+eliminate the digit number  1
+absolute numbers only return positive number 5.0
+log2 of 2 is  1.0
+log10 of 2 is  0.3010299956639812
+constant pi value  3.141592653589793
+constant e value  2.718281828459045'''
