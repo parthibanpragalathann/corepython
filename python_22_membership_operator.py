a577ce97bf28026f4fa44d4918d7563938bebde1
parthiban <parthibanpragalathann@gmail.com>
2022-01-01 19:14:03 +0530

    add MEMBERSHIP Operator python file 22

--- python_22_membership_operator.py
+++ python_22_membership_operator.py
@@ -10,3 +10,13 @@ print("object 11 in sequence variable a ", 11 in a)
 print("object 10 in sequence variable a ", 10 in a)
 print("object 11 not in sequence variable a ", 11 in a)
 print("object 10 not in sequence variable a ", 10 in a)
+
+'''
+OUTPUT
+a sequence values  [1, 11, 111, 1111]
+object 11 in sequence variable a  True
+object 10 in sequence variable a  False
+object 11 not in sequence variable a  True
+object 10 not in sequence variable a  False
+
+'''
