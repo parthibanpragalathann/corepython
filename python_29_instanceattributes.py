ddb6c5405f397b2f04b2d3961eb17a6fd5d38169
parthiban <parthibanpragalathann@gmail.com>
2022-01-29 17:21:17 +0530

    add Instance Attributes python file 29

--- python_29_instanceattributes.py
+++ python_29_instanceattributes.py
@@ -1,4 +1,4 @@
-#Instance attribute
+#Instance attributes
 # A class is a blueprint for the creation of different objects. When the objects get created to form the class,
 # they no longer depend on the class attribute.
 # Also, the class has no control over the attributes of the instances created.
