13f0e19395ae5a2edd75f5962c6ad6ae83cd6162
parthiban <parthibanpragalathann@gmail.com>
2022-01-29 20:00:47 +0530

    add Multiple Inheritance in Python file 38

---
+++ python_38_multipleinheritance.py
@@ -0,0 +1,34 @@
+# Multiple Inheritance in Python
+
+# When a class is derived from more than one base class it is called multiple Inheritance. The derived class inherits all the features of the base case.
+
+# Source Code
+# Multiple Inheritance
+
+class Father:
+    def fishing(self):
+        print("Fishing in Rivers")
+
+    def chess(self):
+        print("Playing Chess From Father")
+
+
+class Mother:
+    def cooking(self):
+        print("Cooking Food")
+
+    def chess(self):
+        print("Playing Chess From Mother")
+
+
+class Son(Mother,Father):
+    def ride(self):
+        print("Riding Bicycle")
+o = Son()
+o.ride()
+o.fishing()
+o.cooking()
+o.chess()
+
+
+Function Overriding in Python
