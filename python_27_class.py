c5164f5f895995031567be4163c8183aeffa1a9d
parthiban <parthibanpragalathann@gmail.com>
2022-01-29 16:36:48 +0530

    add CLASS python file 27

---
+++ python_27_class.py
@@ -0,0 +1,20 @@
+#python OOPS [ class object class attribute, instance attribute,  isinstance, getattr, Dot notation, setattr, class methods, innstance methods
+#self is instance method, additional attribute, init method(constructor for value initializing call when new instance created)
+#property decorator , getter ,  setter, property method,  class method, static method, data abstraction, data encapsulation ]
+
+
+#Python is an object oriented programming language.Almost everything in Python is an object, with its properties and methods.
+#A Class is like an object constructor, or a "blueprint" for creating objects.
+
+
+# Source Code
+class car():
+    pass
+a = 10
+print(type(a))
+print(type(car))
+tata = car()
+
+print(isinstance(tata, car))
+print(isinstance(a, int))
+print(type(tata))
